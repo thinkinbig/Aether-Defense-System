9aa5b9b2968e0b7d79ebabc8fe0cc090a1c4f91c
thinkinbig <lizeyu1101@gmail.com>
2025-12-18 00:11:09 +0100

    test(integration): add in-process gRPC stock test; configure gopls tags

---
+++ test/integration/promotion/doc.go
@@ -0,0 +1,7 @@
+// Package promotion contains integration tests for the promotion service.
+//
+// Note: Most tests in this directory are guarded by the `integration` build tag.
+// Run them with:
+//
+//	go test -tags=integration ./test/integration/promotion -v
+package promotion
